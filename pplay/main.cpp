91fd9a3e8e23d093582239b2b37e8460ca640860
cpasjuste <cpasjuste@gmail.com>
2018-09-29 16:55:37 +0200

    some stuff

---
+++ pplay/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by cpasjuste on 29/09/18.
+//
+
+#include "c2d.h"
+#include "c2dui.h"
+#include "main.h"
+
+using namespace c2d;
+using namespace c2dui;
+
+Renderer *renderer;
+Input *inp;
+Io *io;
+
+UIMenu *uiMenu;
+UIEmu *uiEmu;
+PNESConfig *config;
+PNESRomList *romList;
+PNESUIStateMenu *uiState;
+
---
+++ pplay/main.h
@@ -0,0 +1,9 @@
+//
+// Created by cpasjuste on 29/09/18.
+//
+
+#ifndef PPLAY_MAIN_H
+#define PPLAY_MAIN_H
+
+
+#endif //PPLAY_MAIN_H
