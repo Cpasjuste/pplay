b5c1578dfd7e78f6720794fb497bc7440076d162
Cpasjuste <cpasjuste@gmail.com>
2021-01-22 09:12:22 +0100

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 source /opt/devkitpro/switchvars.sh
 ./configure --prefix="${PORTLIBS_PREFIX}" \
-    --extra-cflags='-DTCP_MAXSEG=2' \
     --enable-cross-compile --cross-prefix=aarch64-none-elf- --arch=aarch64 \
-    --pkg-config=/opt/devkitpro/portlibs/switch/bin/aarch64-none-elf-pkg-config \
+    --pkg-config=${PORTLIBS_PREFIX}/bin/aarch64-none-elf-pkg-config \
+    --extra-cflags='-D_GNU_SOURCE -O2 -march=armv8-a -mtune=cortex-a57 -mtp=soft -fPIC -ftls-model=local-exec' \
+    --extra-cxxflags='-D_GNU_SOURCE -O2 -march=armv8-a -mtune=cortex-a57 -mtp=soft -fPIC -ftls-model=local-exec' \
+    --extra-ldflags='-fPIE -L${PORTLIBS_PREFIX}/lib -L${DEVKITPRO}/libnx/lib' \
     --disable-shared --enable-static --target-os=linux --enable-pic \
-    --enable-asm --enable-neon --disable-runtime-cpudetect \
+    --enable-asm --enable-neon \
     --enable-libass --enable-libfreetype --enable-libfribidi --enable-libtheora \
     --disable-protocols --enable-protocol='file,http,ftp' \
     --disable-filters --enable-filter='rotate,transpose' \
     --disable-encoders --disable-muxers \
-    --disable-programs --disable-debug --disable-doc
-    #TODO: --enable-encoder=png
+    --disable-programs --disable-debug --disable-doc --disable-runtime-cpudetect --disable-autodetect
 
-    #TODO: fix configure
-    sed 's/#define HAVE_INET_ATON 0/#define HAVE_INET_ATON 1/g' -i config.h
-
+#TODO: fix configure
+sed 's/#define HAVE_INET_ATON 0/#define HAVE_INET_ATON 1/g' -i config.h
+sed 's/#define HAVE_CLOSESOCKET 1/#define HAVE_CLOSESOCKET 0/g' -i config.h
+sed 's/#define HAVE_MMAP 1/#define HAVE_MMAP 0/g' -i config.h
