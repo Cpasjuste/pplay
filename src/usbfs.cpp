4751d5bf26f61f96d4d9be64bb298f1f0e4c05a9
cpasjuste <cpasjuste@gmail.com>
2021-01-26 16:55:03 +0100

    usb: wip

--- src/usbfs.cpp
+++ src/usbfs.cpp
@@ -6,6 +6,7 @@
 static UEvent *g_statusChangeEvent, g_exitEvent;
 static UsbHsFsDevice *g_usbDevices;
 static u32 g_usbDeviceCount;
+static thrd_t g_thread = {nullptr};
 
 int usbThread(void *arg) {
 
@@ -20,13 +21,18 @@ int usbThread(void *arg) {
 
     while (true) {
         rc = waitMulti(&idx, -1, status_change_event_waiter, exit_event_waiter);
-        if (R_FAILED(rc)) continue;
+        if (R_FAILED(rc)) {
+            continue;
+        }
+
         if (g_usbDevices) {
             free(g_usbDevices);
             g_usbDevices = nullptr;
         }
 
-        if (idx == 1) break;
+        if (idx == 1) {
+            break;
+        }
 
         g_usbDeviceCount = usbHsFsGetMountedDeviceCount();
         g_usbDevices = (UsbHsFsDevice *) calloc(g_usbDeviceCount, sizeof(UsbHsFsDevice));
@@ -39,19 +45,21 @@ int usbThread(void *arg) {
 void usbInit() {
 
     Result rc;
-    thrd_t g_thread = {0};
 
     printf("usbInit\n");
-
     usbHsFsSetFileSystemMountFlags(UsbHsFsMountFlags_ShowHiddenFiles | UsbHsFsMountFlags_ReadOnly);
     rc = usbHsFsInitialize(0);
     printf("usbHsFsInitialize: %u\n", rc);
     g_statusChangeEvent = usbHsFsGetStatusChangeUserEvent();
     ueventCreate(&g_exitEvent, true);
     thrd_create(&g_thread, usbThread, nullptr);
-    sleep(2);
-
     printf("usbInit: done\n");
+}
+
+void usbExit() {
+    ueventSignal(&g_exitEvent);
+    thrd_join(g_thread, nullptr);
+    usbHsFsExit();
 }
 
 #endif
